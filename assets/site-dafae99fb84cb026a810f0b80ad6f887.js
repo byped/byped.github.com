369cc4a2423e9f95bafe936139e34fb458180e8e
Graham Babbitt <byped@users.noreply.github.com>
2023-07-19 15:49:29 -0400

    Delete assets directory

--- assets/site-dafae99fb84cb026a810f0b80ad6f887.js
+++
@@ -1,1 +0,0 @@
-(function(){var e;e=jQuery,e(function(){var t;return Shadowbox.init(),t=["mai","lto:","gmba","bbit","t@g","mai","l.com"],e("#email a:first").attr("href",t.join("")).html(t.slice(2).join(""))})}).call(this);
